ca8ee554d12007411c56bb5c61d528253ee799be
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2023-09-05 01:27:40 -0400

    Add documentation to AssignmentTwo.java

--- src/main/java/io/github/manuelosorio/AssignmentTwo.java
+++ src/main/java/io/github/manuelosorio/AssignmentTwo.java
@@ -4,6 +4,12 @@ public class AssignmentTwo {
         int fibonacciResult = fibonacci(10);
         System.out.println("Fibonacci result: " + fibonacciResult);
     }
+
+    /**
+     * Recursive method to calculate the fibonacci sequence
+     * @param n The number used to calculate the fibonacci sequence
+     * @return The final result of the fibonacci sequence
+     */
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
